d90bb103c758c73a7f93fb1905bfcf12a4557d89
Gary <garysiu@outlook.com>
2023-10-20 15:35:08 +0100

    Submitting Octopus Energy project

---
+++ octopus_energy.py
@@ -0,0 +1,65 @@
+# coding: utf8
+import datetime
+import operator
+import requests
+
+from OctopusAgile import Agile
+from octopus_energy_api import oe_api
+
+STANDING_CHARGE_URL = ""
+SHEETY_PUT = ""
+
+account_number = ""
+api_key = ""
+mpan = ""
+serial_number = ""
+
+energy_api = oe_api(account_number,
+                    api_key,
+                    mpan=mpan,
+                    serial_number=serial_number)
+
+# Get consumption, standing charge, and cost per day
+for i in range(1, 0, -1):
+    # Get starting date with next date
+    day_before = i
+    day_after = i - 1
+    start_day = datetime.date.today() - datetime.timedelta(days=day_before)
+    end_day = datetime.date.today() - datetime.timedelta(days=day_after)
+
+    # Get standing charge per day
+    params = {
+        "period_from": start_day,
+        "period_to": end_day
+    }
+    r = requests.get(url=STANDING_CHARGE_URL, params=params)
+    standing_charge_dict = r.json()
+    standing_charge = round(standing_charge_dict["results"][0][
+                                "value_inc_vat"], 2)
+
+    # Using package to get consumption rate
+    c = energy_api.consumption(start_day, end_day)
+    consumption_rate = [sub['consumption'] for sub in c]
+    consumption_day = sum(consumption_rate)
+
+    # Using another package to retrieve rates
+    agile = Agile('N')  # N is the region code for Southern Scotland
+    rate = agile.get_rates(start_day, end_day)['rate_list']
+    rate.reverse()
+
+    # Create list of per half hour consumption multiplied respective rate
+    result = list(map(operator.mul, consumption_rate, rate))
+    cost = f"£{str(round(sum(result)) / 100)}"
+    # print(today)
+    # print(cost)
+
+    # Send data to Google Sheets
+    json = {
+        "octopus": {
+            "date": str(start_day),
+            "consumption": str(consumption_day),
+            "standingCharge": str(standing_charge),
+            "cost": cost
+        }
+    }
+    upload_to_sheet = requests.post(url=SHEETY_PUT, json=json)
