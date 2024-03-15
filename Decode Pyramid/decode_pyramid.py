692320858d6861a3570fba13bee2b6916abd23b3
Gary <garysiu@outlook.com>
2024-03-15 01:40:57 +0000

    Submit decode pyramid exercise

--- Decode Pyramid/decode_pyramid.py
+++ Decode Pyramid/decode_pyramid.py
@@ -6,17 +6,22 @@ def decode(message_file):
     # Staircase function outputs
     # [['1 I'], ['2 dogs', '3 love'], ['4 cats', '5 you', '6 computers']]
     step = 1
-    subsets = []
+    pyramid = []
     while len(message_list) != 0:
         if len(message_list) >= step:
-            subsets.append(message_list[0:step])
+            pyramid.append(message_list[0:step])
             message_list = message_list[step:]
             step += 1
         else:
             return False
-    return subsets
+    # Get last word in each row of pyramid and append to list
+    get_words = [row[-1].split()[1] for row in pyramid]
+    # Join words to form sentence as string
+    decoded_message = " ".join(get_words)
+    return decoded_message
 
 
+# Open txt file with the encoded message
 with open("message.txt", "r") as f:
     file = f.read()
 
