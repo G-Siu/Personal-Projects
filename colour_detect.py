01e94dc49b049e80e77a29cda2e1bd05f3bd3acf
Gary <garysiu@outlook.com>
2024-03-18 14:40:18 +0000

    Submit changes to improve colour_detect

--- colour_detect.py
+++ colour_detect.py
@@ -1,8 +1,21 @@
+from pynput import mouse
 import pyautogui
-import time
+from pynput.keyboard import Key, Listener
 
-while True:
-    time.sleep(3)
-    x, y = pyautogui.position()
-    px = pyautogui.pixel(x, y)
-    print(px)
+
+def on_click(x, y, button, pressed):
+    if button == mouse.Button.left and pressed:
+        px = pyautogui.pixel(x, y)
+        print(px)
+
+
+def on_press(key):
+    if key == Key.esc:
+        return False
+
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+
+with Listener(on_press=on_press) as keyboard_listener:
+    keyboard_listener.join()
